30f111ed85a1e372d302a2d33221806ec235bfff
SerikaYuzuki <ReckySet@outlook.jp>
2022-09-22 04:20:14 +0900

    First Commit

---
+++ Source/FirstPersonShooter/FirstPersonShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstPersonShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSONSHOOTER_API AFirstPersonShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
