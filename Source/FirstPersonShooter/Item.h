f9eef182fea150fa5b57261c2ff9d1bf077a0493
SerikaYuzuki <ReckySet@outlook.jp>
2022-10-10 15:30:47 +0900

    Win to Mac

--- Source/FirstPersonShooter/Item.h
+++ Source/FirstPersonShooter/Item.h
@@ -30,5 +30,7 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* ItemMesh;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* PickupWidget;
 public:
 };
